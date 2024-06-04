36481eb6a6d253fced3fa9b3f3066c1f9cd2c9a1
MARC CHRISTIAN JUNIOR <christianmaurelle@gmail.com>
2024-06-04 20:38:39 +0000

    add

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,11 @@
+import { Head } from 'next/head';
 import styles from '../styles/Home.module.css'
 export default function Home() {
   return (
     <>
+    <head>
+      <title>Accueil</title>
+    </head>
    <h1> Accueil </h1>
    <div className={`${styles.accueil} `}>  </div>
     </>
